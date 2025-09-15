f2dfe3406e77831bb37c7d688d4bd7090830f648
Griffin <omondigriffin4@gmail.com>
2025-09-15 16:58:33 +0300

    Feat: makes the background video visible only on large screens

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -38,12 +38,24 @@ function Home() {
         <Navbar />
         {/* Hero Section */}
         <div className="relative h-screen">
-          <div className="absolute inset-0 overflow-hidden ">
-            <video src={videoBg}
-              autoPlay loop muted
-              className="w-full h-full object-cover"
+          <div className="absolute inset-0 overflow-hidden">
+            {/* Image for small screens */}
+            <img
+              src="https://images.unsplash.com/photo-1601758003122-53c40e686a19"
+              alt="background"
+              className="w-full h-full object-cover block sm:hidden"
+            />
+
+            {/* Video for larger screens */}
+            <video
+              src={videoBg}
+              autoPlay
+              loop
+              muted
+              className="w-full h-full object-cover hidden sm:block"
             />
           </div>
+
           <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
             <div className="max-w-5xl mx-auto px-6 text-center">
               <h1 className="text-5xl md:text-6xl font-bold text-white mb-10">
@@ -82,7 +94,7 @@ function Home() {
             <h2 className="text-3xl font-bold text-center text-blue-600 mb-12">Our Ministries</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
               <div className="text-center rounded-4xl p-4 shadow-2xl">
-                <Users strokeWidth={1.5}className="w-12 h-12 text-blue-600 mx-auto mb-4" />
+                <Users strokeWidth={1.5} className="w-12 h-12 text-blue-600 mx-auto mb-4" />
                 <h3 className="text-xl font-semibold mb-2  mx-auto">Community Outreach</h3>
                 <p className="text-gray-600 font-light">Serving our community through various programs and initiatives.</p>
               </div>
